e44847fe530d938650e04d63398a9779cab27b24
ckr-redlinedatavault <help.ckrdatapoint@gmail.com>
2026-03-20 21:12:28 +0530

    fix: update password reset link to academy.studentforge.in

--- lib/mail.ts
+++ lib/mail.ts
@@ -83,7 +83,7 @@ export const sendApprovalEmail = async (email: string, name: string) => {
 };
 
 export const sendPasswordResetEmail = async (email: string, token: string) => {
-    const resetLink = `https://server.studentforge.in/intern/reset-password?token=${token}`;
+    const resetLink = `https://academy.studentforge.in/intern/reset-password?token=${token}`;
     const html = `
     <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
     <html xmlns="http://www.w3.org/1999/xhtml">
